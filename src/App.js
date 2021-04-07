f70800ac12980acf485cb72c2f23f8d84893d098
AhadAli01 <ahadali101aa@gmail.com>
2021-04-06 23:03:12 -0600

    Add to Github

---
+++ src/App.js
@@ -0,0 +1,35 @@
+import React, { useState, useEffect } from 'react'
+import Header from './components/ui/Header'
+import CharacterGrid from './components/characters/CharacterGrid'
+import Search from './components/ui/Search'
+import './App.css'
+
+
+const App = () => {
+  const [items, setItems] = useState([])
+  const [isLoading, setIsLoading] = useState(true)
+  const [query, setQuery] = useState('')
+
+  useEffect(() => {
+    const fetchItems = async () => {
+      const result = await fetch(`https://www.breakingbadapi.com/api/characters?name=${query}`)
+      const data = await result.json()
+
+      // console.log(data)
+      setItems(data)
+      setIsLoading(false)
+    }
+
+    fetchItems()
+  }, [query])
+
+  return (
+    <div className="container">
+      <Header />
+      <Search getQuery={(q) => setQuery(q)}/>
+      <CharacterGrid isLoading={isLoading} items={items}/>
+    </div>
+  );
+}
+
+export default App;
